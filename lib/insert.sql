4b0f81bd55d4b3e42e470ad114d82b04945938be
Romil <romil.m.kothari@gmail.com>
2023-10-30 19:58:23 -0400

    tests completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 10, "M", "black", "chill", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "F", "black", "crazy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "M", "brown", "chill", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 13, "M", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "black", "crazy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "M", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 9, "F", "brown", "crazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, "M", "brown", "chill", 0);
